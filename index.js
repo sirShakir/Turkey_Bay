4b250bc58b278ab4051dd1db86a4ab5a5fcf7d57
Shakir <43176216+sirShakir@users.noreply.github.com>
2021-11-23 19:13:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ var config = {
     type: Phaser.AUTO,
     //width: 224,
     //height: 224,
+    parent: "game-container",
+    pixalArt: true,
+    physics:{
+        default: 'arcade', 
+        arcade : {
+            debug : true
+        }
+    },
+    zoom: 1,
+    scale: {
+        mode: Phaser.DOM.FIT,
+    },
+    scene: [titleScene,scene1,scene2,scene3]
+    
+};
+
+var game = new Phaser.Game(config);
+
