99504c254abbb8b278e91f9294472d6ebc027e62
Shakir <43176216+sirShakir@users.noreply.github.com>
2021-11-23 19:12:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ var config = {
     type: Phaser.AUTO,
     //width: 224,
     //height: 224,
-    parent: 
-    //width: 224,
-    //width: 224,
-    //width: 224,
-    //width: 224,
-    zzzzzzzzzz
